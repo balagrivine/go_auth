56aa7454bc20fe12e2e5897185b78f48937dc706
balagrivine <balagrivine@gmail.com>
2024-08-01 10:22:50 -0700

    create a function to initialize a database connection

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"database/sql"
+	"os"
+
+	"githb.com/balagrivine/go_auth/internal/database"
+	_"github.com/lib/pq"
+)
+
+type APIConfig struct {
+	DB *database.Queries
+}
+
+// Initializes a database connection
+func InitConfig() (*APIConfig, error) {
+
+	dbURL = os.Getenv("DB_URL")
+
+	// Make a database connection
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	if err = conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	queries := database.New(conn)
+
+	return &APIConfig{
+		DB: queries,
+	}, nil
+}
