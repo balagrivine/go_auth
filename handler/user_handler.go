e63bb6e9927289926c73a8d7386515833aac63f4
balagrivine <balagrivine@gmail.com>
2024-08-10 00:33:14 -0700

    add login logic handler

--- handler/user_handler.go
+++ handler/user_handler.go
@@ -14,7 +14,7 @@ import (
 
 type CreateUser struct {
 	Username string  `json:"username" validate:"required"`
-	Email string     `json:"email" validate:"required, email"`
+	Email string     `json:"email" validate:"required"`
 	Password string  `json:"password" validate:"required"`
 	FirstName string `json:"first_name" validate:"required"`
 	LastName string  `json:"last_name" validate:"required"`
@@ -59,8 +59,10 @@ func HandleCreateUser(apiCfg *config.APIConfig) http.HandlerFunc {
 		if err != nil {
 			if err == ErrUserExists{
 				http.Error(w, "Email already registered", http.StatusConflict)
+				return
 			}
 			http.Error(w, fmt.Sprintf("Error while checking duplicate user: %s", err), http.StatusBadRequest)
+			return
 		}
 
 		pass, err := HashPassword(param.Password)
@@ -87,8 +89,64 @@ func HandleCreateUser(apiCfg *config.APIConfig) http.HandlerFunc {
 		})
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Couldn't create user: %s", err), http.StatusBadRequest)
+			return
 		}
 
 		RespondWithJSON(w, 201, user)
+	}
+}
+
+type LoginUser struct {
+	Email string     `json:"email" validate:"required"`
+	Password string  `json:"password" validate:"required"`
+}
+
+func HandleLoginUser(apiCfg *config.APIConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		var param LoginUser
+
+		// Decode the request body into the param variable
+		if err := decoder.Decode(&param); err != nil {
+			http.Error(w, fmt.Sprintf("Error while parsing JSON: %s", err), http.StatusBadRequest)
+			return
+		}
+
+		validate := validator.New()
+
+		if err := validate.Struct(param); err != nil {
+			http.Error(w, fmt.Sprintf("Validation failed: %s", err), http.StatusBadRequest)
+			return
+		}
+
+		user, err := apiCfg.DB.GetUserByEmail(r.Context(), param.Email)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, fmt.Sprintf("Invalid email or username"), http.StatusUnauthorized)
+			}
+			http.Error(w, fmt.Sprintf("Error checking user existence: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		// Validate provided password against the stored hash
+		if !CheckPasswordHash(param.Password, user.Password) {
+			http.Error(w, fmt.Sprintf("Invalid email or password"), http.StatusUnauthorized)
+			return
+		}
+
+		// Create access token for the user
+		token, err := createAccessToken(param.Email)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error creating access token: %s", err), http.StatusInternalServerError)
+		}
+
+		response := map[string]interface{} {
+			"user_id":      user.ID,
+			"token_type":   "Bearer",
+			"access_token": token,
+			"verified":     user.Verified,
+		}
+
+		RespondWithJSON(w, 200, response)
 	}
 }
